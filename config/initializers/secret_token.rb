47d224e2f03eb6a5dcc66de36b9e5fd4112c7859
Denis Kishinevsky <kishinevskyd@ottawasenators.com>
2012-04-30 11:21:04 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Contact::Application.config.secret_token = '29576425e41f08b0af9df3bcdfb4fb7bafd285182e59df48b0f83206c805eae6e4b550e345d8d7daa36566c750dccdbb64684318839b4bba593809a3d2074d83'
